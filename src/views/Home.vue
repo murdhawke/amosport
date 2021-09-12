2bf3ed94b23d7f13753c6abe30e1e0798c1e422e
murdhawke <amoscheruiyot22@gmail.com>
2021-09-13 02:35:07 +0300

    Added projects section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <section class="container">
-      <div class="container is-max-desktop">
+      <div class="container is-mobile">
         <section class="hero  is-medium">
           <div class="hero-body">
             <div class="columns">
               <div class="me ">
                 <figure class="image" >
-                  <img class="is-rounded " src="../assets/avatar.png" >
+                  <img class="is-rounded is-128x128 " src="../assets/avatar.png" >
                 </figure>
               </div>
               <div class="fulltext ">
@@ -24,7 +24,38 @@
                 </div>
               </div>
             </div>
-            <div class="is-divider" data-content="OR"></div>
+          </div>
+        </section>
+        <div class="is-divider" data-content="OR"></div>
+        <section class="work">
+          <div class="columns is-max-desktop">
+            <div class="column">
+              <figure class="image is-rounded is-128x128">
+                <img src="../assets/foota.svg" alt="">
+                <p class="captions">Foota:</p>
+                <p class="subcaption"> Your accurate statistics App</p>
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-rounded is-128x128">
+                <img src="../assets/covidata.svg" alt="">
+                <p class="captions">Covidata: </p>
+                <p class="subcaption">Daily Covid-19 updates</p>
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-rounded is-128x128">
+                <img src="../assets/farmika.svg" alt="">
+                <p class="captions">Farmika: </p>
+                <p class="subcaption"> Farming at your fingertips</p>
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-rounded is-128x128">
+                <img src="../assets/movidb.svg" alt="">
+                <p class="captions">Movidb: </p>
+                <p class="subcaption">The movie wikipedia</p>
+              </figure></div>
           </div>
         </section>
       </div>
@@ -52,5 +83,30 @@ export default {
   margin-right: 10px;
   margin-left: 5px;
 
+}
+.work {
+  width: 60vw;
+  margin: auto;
+  display: flex;
+}
+.captions {
+  font-size: 20px;
+  font-weight: 700;
+  color: #6050dc;
+  text-align: center;
+  padding: 0.5rem;
+}
+.subcaption{
+  font-size: 18px;
+  font-weight: 500;
+  color: inherit;
+  text-align: center;
+}
+.column {
+  margin: 100px 0px 100px 0px;
+  
+}
+.columns {
+  width: 60vw;
 }
 </style>
